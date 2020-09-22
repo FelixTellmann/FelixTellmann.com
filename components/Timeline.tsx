db2d53b2e9feb0c1c445e6cecafcc4ce382e47b5
FelixTellmann <felix@tellmann.co.za>
2020-09-22 15:36:05 +0200

    Added Timeline  TODO - ADd content to Timeline.tsx

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,41 +1,76 @@
-import { FC } from 'react';
+import { FC, Fragment, useEffect, useState } from 'react';
 import Box from './Box';
 import Text from './Text';
 // @ts-ignore
 import CheckIcon from 'public/icons/CheckIcon.svg';
 import Hr from './Hr';
+import Button from './Button';
+import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
 
+type TimelineItemProps = {
+  headline: string
+  description: string
+}
+
+type TimelineGroupProps = {
+  title: string
+  items: TimelineItemProps[]
+}
 type TimelineProps = {
-  data: {
-    title: string
-    items: {
-      headline: string
-      description: string
-    }[]
-  }[]
+  title?: string
+  preview?: TimelineGroupProps[]
+  data: TimelineGroupProps[]
 };
 
-export const Timeline: FC<TimelineProps> = ({ data }) => {
+export const TimelineItem: FC<TimelineItemProps> = ({ headline, description }) => {
   return <>
-    {
-      data.map(({ title, items }, index) => (
-        <Box key={title}>
-          <Text as="h3" fontSize={[20, 4]} fontWeight={700} lineHeight={1.4} mb={10}>{title}</Text>
-          <Box as={'ul'}>
-            {items.map(({ headline, description }) => (
-              <Box key={headline} as={'li'}>
-                <Box d={'flex'} mb={1} align={'center'}>
-                  <CheckIcon style={{ margin: `0 12px 0 8px`, color: `#00bfa5` }} />
-                  <Text as="h4" lineHeight={1.4} fontWeight={600}>{headline}</Text>
-                </Box>
-                <Text as="p" color={'--color-text'} ml={36} mb={3} lineHeight={1.6}>{description}</Text>
-              </Box>
-            ))}
-          </Box>
-          {data.length - 1 !== index ? <Hr /> : ''}
-        </Box>
-      ))
-    }
+    <Box key={headline} as={'li'}>
+      <Box d={'flex'} mb={1} align={'center'}>
+        <CheckIcon style={{ margin: `0 12px 0 8px`, color: `var(--color-green)` }} />
+        <Text as="h4" lineHeight={1.4} fontWeight={600}>{headline}</Text>
+      </Box>
+      <Text as="p" color={'--color-text'} ml={36} mb={3} lineHeight={1.6}>{description}</Text>
+    </Box>
+  </>;
+};
+
+export const TimelineGroup: FC<TimelineGroupProps> = ({ title, items }) => {
+  return <>
+    <Box as='section' key={title}>
+      <Text as="h3" fontSize={[20, 4]} fontWeight={700} lineHeight={1.4} mb={10}>{title}</Text>
+      <Box as={'ul'}>
+        {items.map(props => <TimelineItem {...props} />)}
+      </Box>
+    </Box>
+  </>;
+};
+
+export const Timeline: FC<TimelineProps> = ({ title, data, preview }) => {
+  const [fullTimeline, toggleFullTimeline] = useState(false);
+  const [timelineData, setTimelineData] = useState(preview ? preview : data);
+  
+  useEffect(() => {
+    fullTimeline ? setTimelineData(data) : setTimelineData(preview ? preview : data);
+  }, [fullTimeline]);
+  
+  return <>
+    {title ? <Text as="h2" fontSize={[30, 36]} fontWeight={700} lineHeight={1.2} mb={3}>      {title}    </Text> : ''}
+    {timelineData.map(({ title, items }, index) => (
+      <Fragment key={title}>
+        <TimelineGroup title={title} items={items} />
+        {timelineData.length - 1 !== index ? <Hr /> : ''}
+      </Fragment>
+    ))}
+    {preview
+     ? <Button title={fullTimeline ? <>Show less &nbsp;&nbsp;<FiChevronUp /></> : <>Show more &nbsp;&nbsp; <FiChevronDown /></>}
+               onClick={() => toggleFullTimeline(!fullTimeline)}
+               secondary
+               d={'flex'}
+               mx={'auto'}
+               my={4}
+               fontSize={1}
+               fontWeight={600} />
+     : ''}
   </>;
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,53 @@
-import { FC, useState } from 'react';
-import Box from '../components/Box';
+import { FC } from 'react';
 import Text from '../components/Text';
 import Timeline from '../components/Timeline';
-import Button from '../components/Button';
-import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
 
 export const Index: FC = () => {
   
+  const INTRO = {
+    title: `Hi, i'm Felix Tellmann`,
+    description: `I'm a freelancing web developer, writer and entrepreneur living in Cape Town. I teach people coding and have been building things
+        for the web for the last 6 years.`
+  };
   const TIMELINE_CURRENT = [
     {
       title: `2020`,
       items: [
         {
-          headline: 'Moved to Cape Town',
-          description: 'I\'m excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.'
+          headline: `Moved to Cape Town`,
+          description: `I'm excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.`
         },
         {
-          headline: 'Moved to Cape Town',
-          description: 'I\'m excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.'
+          headline: `Moved to Cape Town`,
+          description: `I'm excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.`
         },
         {
-          headline: 'Moved to Cape Town',
-          description: 'I\'m excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.'
+          headline: `Moved to Cape Town`,
+          description: `I'm excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.`
         },
         {
-          headline: 'Moved to Cape Town',
-          description: 'I\'m excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.'
+          headline: `Moved to Cape Town`,
+          description: `I'm excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.`
         }
       ]
     }, {
       title: `2019`,
       items: [
         {
-          headline: 'Moved to Cape Town',
-          description: 'I\'m excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.'
+          headline: `Moved to Cape Town`,
+          description: `I'm excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.`
         },
         {
-          headline: 'Moved to Cape Town',
-          description: 'I\'m excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.'
+          headline: `Moved to Cape Town`,
+          description: `I'm excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.`
         },
         {
-          headline: 'Moved to Cape Town',
-          description: 'I\'m excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.'
+          headline: `Moved to Cape Town`,
+          description: `I'm excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.`
         },
         {
-          headline: 'Moved to Cape Town',
-          description: 'I\'m excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.'
+          headline: `Moved to Cape Town`,
+          description: `I'm excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.`
         }
       ]
     }
@@ -55,20 +57,20 @@ export const Index: FC = () => {
       title: `2018`,
       items: [
         {
-          headline: 'Moved to Cape Town',
-          description: 'I\'m excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.'
+          headline: `Moved to Cape Town`,
+          description: `I'm excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.`
         },
         {
-          headline: 'Moved to Cape Town',
-          description: 'I\'m excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.'
+          headline: `Moved to Cape Town`,
+          description: `I'm excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.`
         },
         {
-          headline: 'Moved to Cape Town',
-          description: 'I\'m excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.'
+          headline: `Moved to Cape Town`,
+          description: `I'm excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.`
         },
         {
-          headline: 'Moved to Cape Town',
-          description: 'I\'m excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.'
+          headline: `Moved to Cape Town`,
+          description: `I'm excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.`
         }
       ]
     },
@@ -76,54 +78,29 @@ export const Index: FC = () => {
       title: `2017`,
       items: [
         {
-          headline: 'Moved to Cape Town',
-          description: 'I\'m excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.'
+          headline: `Moved to Cape Town`,
+          description: `I'm excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.`
         },
         {
-          headline: 'Moved to Cape Town',
-          description: 'I\'m excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.'
+          headline: `Moved to Cape Town`,
+          description: `I'm excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.`
         },
         {
-          headline: 'Moved to Cape Town',
-          description: 'I\'m excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.'
+          headline: `Moved to Cape Town`,
+          description: `I'm excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.`
         },
         {
-          headline: 'Moved to Cape Town',
-          description: 'I\'m excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.'
+          headline: `Moved to Cape Town`,
+          description: `I'm excited to help grow the Next.js community and continue building the optimal workflow for front-end developers.`
         }
       ]
     }
   ];
-  const [fullTimeline, toggleFullTimeline] = useState(false);
   
   return <>
-    <Box id="intro" mb={[3, 4]}>
-      <Text as="h1" fontSize={[36, 6]} fontWeight={700} lineHeight={1.2} mb={10}>
-        
-        Hi, i'm Felix Tellmann
-      
-      </Text>
-      <Text as="p" fontSize={2} lineHeight={1.6} color={'--color-text'}>
-        
-        I'm a freelancing web developer, writer and entrepreneur living in Cape Town. I teach people coding and have been building things
-        for the web for the last 6 years.
-      
-      </Text>
-    </Box>
-    <Box id="timeline">
-      <Text as="h2" fontSize={[30, 36]} fontWeight={700} lineHeight={1.2} mb={3}>
-        Timeline
-      </Text>
-      <Timeline data={fullTimeline ? [...TIMELINE_CURRENT, ...TIMELINE_PAST] : TIMELINE_CURRENT} />
-      <Button title={fullTimeline ? <>Show less &nbsp;&nbsp;&nbsp;<FiChevronUp /></> : <>Show more &nbsp;&nbsp;&nbsp;<FiChevronDown /></>}
-              onClick={() => toggleFullTimeline(!fullTimeline)}
-              secondary
-              d={'flex'}
-              mx={'auto'}
-              my={4}
-              fontSize={1}
-              fontWeight={600} />
-    </Box>
+    <Text as="h1" fontSize={[36, 6]} fontWeight={700} lineHeight={1.2} mb={10}>{INTRO.title}</Text>
+    <Text as="p" fontSize={2} lineHeight={1.6} color={'--color-text'} mb={[3, 4]}>{INTRO.description}</Text>
+    <Timeline title="Timeline" preview={TIMELINE_CURRENT} data={[...TIMELINE_CURRENT, ...TIMELINE_PAST]} />
   </>;
 };
 
