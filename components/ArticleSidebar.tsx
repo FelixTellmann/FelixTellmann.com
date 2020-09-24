a9f9990c6f2bbfd630bf4ef1347aa42964f750d8
FelixTellmann <felix@tellmann.co.za>
2020-09-24 21:28:54 +0200

    Added Timeline  Working on Blog Page  Added heading menu

--- components/ArticleSidebar.tsx
+++ components/ArticleSidebar.tsx
@@ -61,6 +61,7 @@ export const ArticleSidebar: FC<ArticleSidebarProps> = ({ showHeadings, headings
   const [observerA, setObserverA] = useState<any>();
   const [observerB, setObserverB] = useState<any>();
   const [showSidebar, setShowSidebar] = useState(false);
+  const [hideSidebarOverwrite, setHideSidebarOverwrite] = useState(false);
   
   useEffect(() => {
     setObserverA(new IntersectionObserver(
