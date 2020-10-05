3e4d6226bb3d8504378abb3fa45ba7e01be9df77
FelixTellmann <felix@tellmann.co.za>
2020-10-05 20:11:34 +0200

    Favicon based on colorTheme

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import Button from './Button';
 import { FiAperture, FiMoon, FiSun } from 'react-icons/fi';
 import useColorTheme from 'use-color-theme';
 import LinkBlock from './LinkBlock';
+import Head from 'next/head';
 
 type HeaderProps = {
   logo: {
@@ -22,6 +23,25 @@ export const Header: FC<HeaderProps> = ({ logo, nav }) => {
   const colorTheme = useColorTheme('light-theme', { classNames: ['light-theme', 'dark-theme', 'blue-theme'] });
   
   return <>
+    <Head>
+      <link rel="apple-touch-icon" sizes="57x57" href={`/favicon/${colorTheme.value}/apple-icon-57x57.png`} />
+      <link rel="apple-touch-icon" sizes="60x60" href={`/favicon/${colorTheme.value}/apple-icon-60x60.png`} />
+      <link rel="apple-touch-icon" sizes="72x72" href={`/favicon/${colorTheme.value}/apple-icon-72x72.png`} />
+      <link rel="apple-touch-icon" sizes="76x76" href={`/favicon/${colorTheme.value}/apple-icon-76x76.png`} />
+      <link rel="apple-touch-icon" sizes="114x114" href={`/favicon/${colorTheme.value}/apple-icon-114x114.png`} />
+      <link rel="apple-touch-icon" sizes="120x120" href={`/favicon/${colorTheme.value}/apple-icon-120x120.png`} />
+      <link rel="apple-touch-icon" sizes="144x144" href={`/favicon/${colorTheme.value}/apple-icon-144x144.png`} />
+      <link rel="apple-touch-icon" sizes="152x152" href={`/favicon/${colorTheme.value}/apple-icon-152x152.png`} />
+      <link rel="apple-touch-icon" sizes="180x180" href={`/favicon/${colorTheme.value}/apple-icon-180x180.png`} />
+      <link rel="icon" type="image/png" sizes="192x192" href={`/favicon/${colorTheme.value}/android-icon-192x192.png`} />
+      <link rel="icon" type="image/png" sizes="32x32" href={`/favicon/${colorTheme.value}/favicon-32x32.png`} />
+      <link rel="icon" type="image/png" sizes="96x96" href={`/favicon/${colorTheme.value}/favicon-96x96.png`} />
+      <link rel="icon" type="image/png" sizes="16x16" href={`/favicon/${colorTheme.value}/favicon-16x16.png`} />
+      <link rel="manifest" href={`/favicon/${colorTheme.value}/manifest.json`} />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="msapplication-TileImage" content={`/favicon/${colorTheme.value}/ms-icon-144x144.png`} />
+      <meta name="theme-color" content="#ffffff" />
+    </Head>
     <Box as="header"
          mb={4}
          mt={[0, 4]}
