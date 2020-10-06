9b30b648d11326f91efd89ae1bc71daa03a5c670
FelixTellmann <felix@tellmann.co.za>
2020-10-06 18:24:06 +0200

    Reformatting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,16 @@
-import { AppProps } from 'next/app';
-import { FC } from 'react';
-import GoogleFonts from 'next-google-fonts';
-import { BreakpointProvider } from 'use-styled-system';
-import Header from '../components/Header';
-import { FiFacebook, FiGithub, FiMail, FiTwitter } from 'react-icons/fi';
-import Footer from '../components/Footer';
-import { Loading } from '../components/Loading';
-import { DefaultSeo } from 'next-seo';
-import 'reset-css/sass/_reset.scss';
-import 'styles/theme.scss';
+import { AppProps } from 'next/app'
+import { FC } from 'react'
+import GoogleFonts from 'next-google-fonts'
+import { BreakpointProvider } from 'use-styled-system'
+import Header from '../components/Header'
+import { FiFacebook, FiGithub, FiMail, FiTwitter } from 'react-icons/fi'
+import Footer from '../components/Footer'
+import { Loading } from '../components/Loading'
+import { DefaultSeo } from 'next-seo'
+import 'reset-css/sass/_reset.scss'
+import 'styles/theme.scss'
 
 export const _App: FC<AppProps> = ({ pageProps, Component }) => {
-  
   return (
     <>
       <style jsx global>{`
@@ -86,7 +85,7 @@ export const _App: FC<AppProps> = ({ pageProps, Component }) => {
           position: relative;
         }
       `}</style>
-      
+
       <BreakpointProvider breakPoints={[0, 600, 900, 1200]}>
         <DefaultSeo
           title="Felix Tellmann - Front-end Engineer"
@@ -96,12 +95,12 @@ export const _App: FC<AppProps> = ({ pageProps, Component }) => {
             locale: 'en_IE',
             url: 'https://www.felixtellmann.com/',
             site_name: 'Felix Tellmann',
-            title:"Felix Tellmann - Front-end Engineer",
-            description:"Creator of things that live on the internet - Web developer, writer and entrepreneur.",
+            title: 'Felix Tellmann - Front-end Engineer',
+            description: 'Creator of things that live on the internet - Web developer, writer and entrepreneur.',
             images: [
               {
                 url: 'https://www.felixtellmann.com/images/og-default.jpg',
-                alt: "Felix Tellmann - Front-end Engineer",
+                alt: 'Felix Tellmann - Front-end Engineer',
                 width: 1200,
                 height: 630
               }
@@ -121,36 +120,77 @@ export const _App: FC<AppProps> = ({ pageProps, Component }) => {
             { title: 'Home', href: '/' },
             { title: 'Blog', href: '/blog' },
             { title: 'About', href: '/about' }
-          ]} />
+          ]}
+        />
         <div className="page">
           <Component {...pageProps} />
         </div>
         <Footer
           socialNav={[
             {
-              title: <FiGithub style={{ stroke: 'url(#gradient) currentColor' }} />,
-              href: 'https://github.com/FelixTellmann', target: '_blank'
+              title: (
+                <FiGithub
+                  style={{
+                    stroke: 'url(#gradient) currentColor'
+                  }}
+                />
+              ),
+              href: 'https://github.com/FelixTellmann',
+              target: '_blank'
             },
             {
-              title: <FiFacebook style={{ stroke: 'url(#gradient) currentColor' }} />,
-              href: 'https://www.facebook.com/felixtellmann', target: '_blank'
+              title: (
+                <FiFacebook
+                  style={{
+                    stroke: 'url(#gradient) currentColor'
+                  }}
+                />
+              ),
+              href: 'https://www.facebook.com/felixtellmann',
+              target: '_blank'
             },
             {
-              title: <FiTwitter style={{ stroke: 'url(#gradient) currentColor' }} />,
-              href: 'https://twitter.com/FelixTellmann', target: '_blank'
+              title: (
+                <FiTwitter
+                  style={{
+                    stroke: 'url(#gradient) currentColor'
+                  }}
+                />
+              ),
+              href: 'https://twitter.com/FelixTellmann',
+              target: '_blank'
             },
             {
-              title: <FiMail style={{ stroke: 'url(#gradient) currentColor' }} />,
-              href: 'mailto:hi@felixtellmann.com', target: '_blank'
+              title: (
+                <FiMail
+                  style={{
+                    stroke: 'url(#gradient) currentColor'
+                  }}
+                />
+              ),
+              href: 'mailto:hi@felixtellmann.com',
+              target: '_blank'
             }
           ]}
           footerNav={[
-            { title: 'visit-my-old-site', href: 'https://old-tellmann-site.vercel.app/webdesign.html', target: '_blank' }/*
+            {
+              title: 'visit-my-old-site',
+              href: 'https://old-tellmann-site.vercel.app/webdesign.html',
+              target: '_blank'
+            } /*
             { title: '/uses', href: '/uses' },
             { title: '/photos', href: '/photos' },
             { title: '/newsletter', href: '/newsletter' }*/
-          ]} />
-        <svg aria-hidden="true" focusable="false" style={{ width: 0, height: 0, position: `absolute` }}>
+          ]}
+        />
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          style={{
+            width: 0,
+            height: 0,
+            position: `absolute`
+          }}>
           <linearGradient id="gradient" gradientTransform="rotate(65)">
             <stop offset="25.28%" stopColor="var(--color-gradient-1)" />
             <stop offset="57.7%" stopColor="var(--color-gradient-2)" />
@@ -159,7 +199,7 @@ export const _App: FC<AppProps> = ({ pageProps, Component }) => {
         </svg>
       </BreakpointProvider>
     </>
-  );
-};
+  )
+}
 
-export default _App;
+export default _App
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,28 +1,29 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
   }
-  
+
   render() {
     return (
       <Html lang="en">
         <Head>
           <meta name="referrer" content="strict-origin-when-cross-origin" />
-          <meta charSet="UTF-8"/>
+          <meta charSet="UTF-8" />
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-JS229JV27E" />
-          <script dangerouslySetInnerHTML={{
-            __html: `
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
     
             gtag('config', 'G-JS229JV27E');
             `
-          }} />
-          
+            }}
+          />
         </Head>
         <body>
           <script src="/colorTheme.js" />
@@ -30,8 +31,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
